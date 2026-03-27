eb7e42a612e450ebaeeb31cb00c57213db584bde
mbafousu <carolngue06@gmail.com>
2026-03-27 19:49:44 -0400

    fix: match App.css import case for render deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/app.css";
+import "./styles/App.css";
 import "./styles/login.css";
 import "./styles/navbar.css";
 
